a5ab84e23b182e5a4f0a69f23b041d618791f6cc
Kushal <kushalsunkara1947@gmail.com>
2025-09-27 07:45:08 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/About-web/dist/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,

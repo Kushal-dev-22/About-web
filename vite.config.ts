bae1f6dafc2295d630593a0a4ccc35c4aa0bb9ff
Kushal <kushalsunkara1947@gmail.com>
2025-09-27 06:37:39 +0530

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/About-web/',
+  base: process.env.VITE_BASE_PATH ||'/About-web/',
   server: {
     host: "::",
     port: 8080,
